0c6ec1c7d5331259c433091a2aaf9dc471116541
Satang090144 <developer.satang.pdm@gmail.com>
2025-10-17 17:08:40 +0700

    video-controls

--- src/components/VideoHome.jsx
+++ src/components/VideoHome.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Css/VideoHome.css';
 import pdVideo from '../assets/video/01.mp4';
 
 const VideoHome = () => {
+  const [showControls, setShowControls] = useState(false);
+
   return (
     <div className="video-home-container">
+      <h1>video</h1>
       <video
         className="video-home-iframe"
         src={pdVideo}
-        controls
         autoPlay
         muted
         loop
         playsInline
+        controls={showControls} // ใช้ state ควบคุม
+        onLoadedData={(e) => e.target.play()}
+        onClick={() => setShowControls(true)} // แตะวิดีโอแล้วโชว์ controls
       />
     </div>
   );
