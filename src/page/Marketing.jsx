baffe0466f8a63dc684c58c63c05a8dce87f232b
ToelPD27 <developer.toel.pdm@gmail.com>
2025-11-11 14:11:57 +0700

    commit scrollTo

--- src/page/Marketing.jsx
+++ src/page/Marketing.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
 import Preface from "../components/marketings/preface.jsx";
 
 function Marketing() {
+  useEffect(() => {
+    // เลื่อนไปที่ top ของหน้าเมื่อ component โหลด
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // ถ้าอยากเลื่อนแบบ smooth
+    });
+  }, []);
   return (
     <main>
       <Preface />
