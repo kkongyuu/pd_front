15c4acc888cd838bcac42fb27ed5a9000b4cc89d
ToelPD27 <developer.toel.pdm@gmail.com>
2026-06-05 15:45:01 +0700

    delete  teamMember number 3

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -13,7 +13,7 @@ import num10 from '../assets/img/profile/05.jpg'
 const teamMembers = [
   { name: "คุณเนตร แก้วปัญญา แซ่กอ", img: num1, position: "Manager Develop Ai" },
   { name: "คุณก้อง ธิติ ทรัพธนาธร", img: num2, position: "Manager Develop Ai" },
-  { name: "คุณมาย อัจฉรา โคตรก่ำ", img: num3, position: "Manager Develop Ai" },
+  // { name: "คุณมาย อัจฉรา โคตรก่ำ", img: num3, position: "Manager Develop Ai" },
   { name: "คุณฐานิตา กิจศิริ", img: num4, position: "Manager Develop Ai" },
   { name: "คุณน๊อต นฤเบศปริญญานันทการ", img: num5, position: "Manager Develop Ai" },
   { name: "คุณส้มสุชาดา บุตรรม", img: num6, position: "Manager Develop Ai" },
